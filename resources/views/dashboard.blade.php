8c7ad77e5250764f48454480d1842278de3fb26a
Sergio Cambara <scambaradorbigny@gmail.com>
2023-10-11 16:41:45 -0400

    primer proyecto

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+{{-- 
+@extends('adminlte::page')
+@section('title', 'e-commerce')
+
+@section('content_header')
+   <h1>
+
+    <!-- Navigation Links -->
+    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+        <x-nav-link href="{{ route('usuario.inicio') }}" :active="request()-> 
+      routeIs('usuario.inicio')"> 
+            {{ __('USUARIO') }}
+        </x-nav-link>
+    </div>
+
+   </h1>
+
+@stop
+
+@section('content')
+    <p>Bienvenido al panel de administración de e-commerce.</p>
+    
+@stop --}}
